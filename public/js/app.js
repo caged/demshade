5bd4f14bbb606d22d5ce0403cb7f39624f325d28
Caged <justin@labratrevenge.com>
2016-12-18 21:31:00 -0800

    spike out start of map

--- public/js/app.js
+++ public/js/app.js
@@ -8,35 +8,22 @@ var margin = { top: 20, right: 20, bottom: 20, left: 50 },
     width = ewidth - margin.left - margin.right,
     height = eheight - margin.top - margin.bottom;
 
-// Generate fake data
-var random = d3.randomNormal(0, 0.2),
-    data = d3.range(300).map(function () {
-  return [random() + Math.sqrt(3), random() + 1];
-});
+var path = d3.geoPath().projection(null);
 
-var x = d3.scaleLinear().domain(d3.extent(data, function (d) {
-  return d[0];
-})).range([0, width]);
+// const x = d3.scaleLinear()
+//   .domain(d3.extent(data, d => d[0]))
+//   .range([0, width])
 
-var y = d3.scaleLinear().domain(d3.extent(data, function (d) {
-  return d[1];
-})).range([height, 0]);
-
-var xax = d3.axisTop(x),
-    yax = d3.axisLeft(y);
+// const y = d3.scaleLinear()
+//   .domain(d3.extent(data, d => d[1]))
+//   .range([height, 0])
 
 var vis = el.append("svg").attr("width", width + margin.left + margin.right).attr("height", height + margin.top + margin.bottom).append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-vis.append("g").call(xax);
-
-vis.append("g").call(yax);
-
-var nodes = vis.append("g").attr("class", "nodes");
-
-nodes.selectAll(".node").data(data).enter().append("circle").attr("r", 3).attr("cx", function (d) {
-  return x(d[0]);
-}).attr("cy", function (d) {
-  return y(d[1]);
-}).style("fill", function () {
-  return "hsl(" + Math.random() * 360 + ", 50%, 50%)";
-});
+// d3.json("https://d3js.org/us-10m.v1.json", function(error, us) {
+//   if (error) throw error;
+//
+//   context.beginPath();
+//   path(topojson.mesh(us));
+//   context.stroke();
+// });
