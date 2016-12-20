0e6a0bd527672ea938a68a1632323ead5822269f
Caged <justin@labratrevenge.com>
2016-12-20 08:37:03 -0800

    use sliders to debug extent fitting

--- js/app.js
+++ js/app.js
@@ -2,9 +2,19 @@ const el = d3.select(".js-vis"),
     ewidth = parseFloat(el.style("width")),
     eheight = parseFloat(el.style("height"))
 
-const margin = { top: 20, right: 0, bottom: 0, left: 0 },
+const margin = { top: 0, right: 0, bottom: 0, left: 0 },
     width = ewidth - margin.left - margin.right,
     height = eheight - margin.top - margin.bottom
+
+var projection = d3.geoConicConformal()
+  .parallels([43, 45.5])
+  .rotate([120.5, 0])
+
+var path = d3.geoPath()
+  .projection(projection)
+
+var pathel = d3.select(null)
+var sliders = d3.selectAll('.js-slider')
 
 const vis = el.append("svg")
   .attr("width", width + margin.left + margin.right)
@@ -15,21 +25,27 @@ const vis = el.append("svg")
 d3.json("/oregon-topo.json", function(error, oregon) {
   if (error) throw error;
   const feature = topojson.feature(oregon, oregon.objects.state)
-  // projection.fitSize([width, height], feature)
 
-  var path = d3.geoPath()
-    .projection(d3.geoConicConformal()
-    .parallels([43, 45.5])
-    .rotate([120.5, 0])
-    .fitExtent([[160, 20], [width, height]], feature))
+  projection.fitExtent([[0, 0], [width, height]], feature)
 
-  vis.append('path')
+  pathel = vis.append('path')
     .datum(feature)
     .attr('class', 'state')
     .attr('d', path)
 
-  //
-  // context.beginPath();
-  // path(topojson.mesh(us));
-  // context.stroke();
+  function reproject(el) {
+    const parent = d3.select(this.parentNode)
+    const x = parent.select('.js-slider[data-extent="x0"]').node().value
+    const y = parent.select('.js-slider[data-extent="y0"]').node().value
+
+    projection
+      .fitExtent([[x, y], [width, height]], feature)
+
+    pathel.transition()
+      .attr('d', path)
+
+  }
+
+  sliders.on('change', reproject)
+
 });
--- public/js/app.js
+++ public/js/app.js
@@ -4,23 +4,36 @@ var el = d3.select(".js-vis"),
     ewidth = parseFloat(el.style("width")),
     eheight = parseFloat(el.style("height"));
 
-var margin = { top: 20, right: 0, bottom: 0, left: 0 },
+var margin = { top: 0, right: 0, bottom: 0, left: 0 },
     width = ewidth - margin.left - margin.right,
     height = eheight - margin.top - margin.bottom;
+
+var projection = d3.geoConicConformal().parallels([43, 45.5]).rotate([120.5, 0]);
+
+var path = d3.geoPath().projection(projection);
+
+var pathel = d3.select(null);
+var sliders = d3.selectAll('.js-slider');
 
 var vis = el.append("svg").attr("width", width + margin.left + margin.right).attr("height", height + margin.top + margin.bottom).append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 d3.json("/oregon-topo.json", function (error, oregon) {
   if (error) throw error;
   var feature = topojson.feature(oregon, oregon.objects.state);
-  // projection.fitSize([width, height], feature)
 
-  var path = d3.geoPath().projection(d3.geoConicConformal().parallels([43, 45.5]).rotate([120.5, 0]).fitExtent([[160, 20], [width, height]], feature));
+  projection.fitExtent([[0, 0], [width, height]], feature);
 
-  vis.append('path').datum(feature).attr('class', 'state').attr('d', path);
+  pathel = vis.append('path').datum(feature).attr('class', 'state').attr('d', path);
 
-  //
-  // context.beginPath();
-  // path(topojson.mesh(us));
-  // context.stroke();
+  function reproject(el) {
+    var parent = d3.select(this.parentNode);
+    var x = parent.select('.js-slider[data-extent="x0"]').node().value;
+    var y = parent.select('.js-slider[data-extent="y0"]').node().value;
+
+    projection.fitExtent([[x, y], [width, height]], feature);
+
+    pathel.transition().attr('d', path);
+  }
+
+  sliders.on('change', reproject);
 });
